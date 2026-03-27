e0f3ad8bed852fc8992668b91a49ffcf3dd2a5fb
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-03-27 19:44:24 -0400

    mkshell improvements

--- lib/mkShell.nix
+++ lib/mkShell.nix
@@ -2,8 +2,6 @@
   lib,
   nuenv,
 }:
-# A special kind of derivation that is only meant to be consumed by the
-# nix-shell.
 lib.extendMkDerivation {
   constructDrv = nuenv.mkDerivation;
 
@@ -16,22 +14,27 @@ lib.extendMkDerivation {
     _finalAttrs:
     {
       name ? "nix-shell",
-      # a list of packages to add to the shell environment
+
+      # Packages to add to the shell environment
       packages ? [ ],
-      # propagate all the inputs from the given derivations
+
+      # Propagate all inputs from the listed derivations
       inputsFrom ? [ ],
       ...
     }@attrs:
     let
+      # Merge a single input-list attribute from all inputsFrom derivations,
+      # excluding the derivations themselves.
       mergeInputs =
-        name:
-        (attrs.${name} or [ ])
-        ++
-          # 1. get all `{build,nativeBuild,...}Inputs` from the elements of `inputsFrom`
-          # 2. since that is a list of lists, `flatten` that into a regular list
-          # 3. filter out of the result everything that's in `inputsFrom` itself
-          # this leaves actual dependencies of the derivations in `inputsFrom`, but never the derivations themselves
-          (lib.subtractLists inputsFrom (lib.flatten (lib.catAttrs name inputsFrom)));
+        attrName:
+        (attrs.${attrName} or [ ])
+        ++ (lib.subtractLists inputsFrom (lib.flatten (lib.catAttrs attrName inputsFrom)));
+
+      # Accept either the nuenv-style name ("build") or the nixpkgs-style
+      # work out of the box.
+      resolvePhase =
+        nuenvName: nixpkgsName: fallback:
+        attrs.${nuenvName} or attrs.${nixpkgsName} or fallback;
     in
     {
       inherit name;
@@ -41,21 +44,53 @@ lib.extendMkDerivation {
       propagatedBuildInputs = mergeInputs "propagatedBuildInputs";
       propagatedNativeBuildInputs = mergeInputs "propagatedNativeBuildInputs";
 
+      # Concatenate shellHooks from all inputsFrom derivations (deepest first),
+      # then the shell's own hook
       shellHook = lib.concatStringsSep "\n" (
         lib.catAttrs "shellHook" (lib.reverseList inputsFrom ++ [ attrs ])
       );
 
-      # Map buildPhase to build for nuenv
-      build =
-        attrs.build or ''
+      unpack = resolvePhase "unpack" "unpackPhase" "";
+      patch = resolvePhase "patch" "patchPhase" "";
+      configure = resolvePhase "configure" "configurePhase" "";
+      check = resolvePhase "check" "checkPhase" "";
+      install = resolvePhase "install" "installPhase" "";
+      fixup = resolvePhase "fixup" "fixupPhase" "";
+      installCheck = resolvePhase "installCheck" "installCheckPhase" "";
+      dist = resolvePhase "dist" "distPhase" "";
+
+      # The build/buildPhase is the most commonly customised phase; provide a
+      # sensible default that records the environment for introspection.
+      build = resolvePhase "build" "buildPhase" ''
+        {
           print "------------------------------------------------------------"
           print " WARNING: the existence of this path is not guaranteed."
           print " It is an internal implementation detail for nuenv.mkShell."
           print "------------------------------------------------------------"
           print ""
-          # Record environment
-          $env | table --expand | save $env.out
-        '';
+          # Record all environment variables for debugging
+          $env | table --expand | save --force $env.out
+        }
+      '';
+
+      preUnpack = resolvePhase "preUnpack" "preUnpack" "";
+      postUnpack = resolvePhase "postUnpack" "postUnpack" "";
+      prePatch = resolvePhase "prePatch" "prePatch" "";
+      postPatch = resolvePhase "postPatch" "postPatch" "";
+      preConfigure = resolvePhase "preConfigure" "preConfigure" "";
+      postConfigure = resolvePhase "postConfigure" "postConfigure" "";
+      preBuild = resolvePhase "preBuild" "preBuild" "";
+      postBuild = resolvePhase "postBuild" "postBuild" "";
+      preCheck = resolvePhase "preCheck" "preCheck" "";
+      postCheck = resolvePhase "postCheck" "postCheck" "";
+      preInstall = resolvePhase "preInstall" "preInstall" "";
+      postInstall = resolvePhase "postInstall" "postInstall" "";
+      preFixup = resolvePhase "preFixup" "preFixup" "";
+      postFixup = resolvePhase "postFixup" "postFixup" "";
+      preInstallCheck = resolvePhase "preInstallCheck" "preInstallCheck" "";
+      postInstallCheck = resolvePhase "postInstallCheck" "postInstallCheck" "";
+      preDist = resolvePhase "preDist" "preDist" "";
+      postDist = resolvePhase "postDist" "postDist" "";
 
       preferLocalBuild = attrs.preferLocalBuild or true;
     };
